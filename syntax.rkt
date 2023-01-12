ca33fa142982ba9a64d3e1567039d029825dd2d1
Li Dzangfan <dzangfan@onmail.com>
2023-01-12 10:26:06 +0800

    Add and pass tests for lwg-lexer

--- syntax.rkt
+++ syntax.rkt
@@ -15,7 +15,7 @@
   (:+ (:or (char-range #\0 #\9)
            (char-range #\a #\z)
            (char-range #\A #\Z)
-           #\-)))
+           #\_)))
 
 (define lwg-lexer
   (lexer-src-pos
@@ -34,3 +34,44 @@
    [(:: symbol (:+ (:: #\. symbol)))
     (token-PROPERTY (string-split lexeme "."))]
    [symbol (token-SYMBOL lexeme)]))
+
+(module+ test
+  (require rackunit)
+  (require racket/match)
+  (define (token->list token)
+    (if (symbol? token)
+        token
+        (list (token-name token)
+              (token-value token))))
+  (define (string->tokens source)
+    (let ([port (open-input-string source)])
+      (let collect-next ([tokens null])
+        (match (lwg-lexer port)
+          [(position-token 'EOF _ _) (reverse tokens)]
+          [(position-token token _ _)
+           (collect-next (cons (token->list token) tokens))]))))
+  (test-case "Simple graph tokenization test"
+    (define source "^x-y>>>>z   \n^x -^t")
+    (check-equal? (string->tokens source)
+                  '(PUSH (SYMBOL "x") MOVE (SYMBOL "y")
+                         (POP+MOVE 3) (SYMBOL "z")
+                         PUSH (SYMBOL "x") MOVE PUSH
+                         (SYMBOL "t"))))
+  (test-case "Simple assignment tokenization test"
+    (define source "x=10 x.y=x x={y=10 z=20}")
+    (check-equal? (string->tokens source)
+                  '((SYMBOL "x") EQ (SYMBOL "10") (PROPERTY ("x" "y"))
+                                 EQ (SYMBOL "x") (SYMBOL "x")
+                                 EQ LEFT-CURLY (SYMBOL "y")
+                                 EQ (SYMBOL "10") (SYMBOL "z")
+                                 EQ (SYMBOL "20") RIGHT-CURLY)))
+  (test-case "Single-quoted string tokenization test"
+    (define source "'hello world!\\n'")
+    (check-equal? (string->tokens source)
+                  '((SINGLE-STR "hello world!\\n"))))
+  (test-case "Double-quoted string tokenization test"
+    (define source "\"hello \\{world\\}\"")
+    (check-equal? (string->tokens source)
+                  '((DOUBLE-STR "hello \\{world\\}")))))
+
+
